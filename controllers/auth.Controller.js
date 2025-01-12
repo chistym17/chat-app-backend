964524446a8ff24a6922fcd8b4bce1275655d5cd
chistym17 <mchisty31@gmail.com>
2025-01-12 22:47:49 +0600

    update

--- controllers/auth.Controller.js
+++ controllers/auth.Controller.js
@@ -1,6 +1,6 @@
 import bcrypt from "bcryptjs";
-import User from "../models/user.model.js";
-import generateTokenAndSetCookie from "../utils/generateToken.js";
+import User from "../models/usermodels.js";
+import generateTokenAndSetCookie from "../utils/generateTokenAndSetCookie.js";
 
 export const signup = async (req, res) => {
   try {
