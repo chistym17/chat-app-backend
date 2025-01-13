25b773ea3870e6dbd4ac678842a3f67ac4684c0c
chistym17 <mchisty31@gmail.com>
2025-01-13 22:44:21 +0600

    token updated

--- server.js
+++ server.js
@@ -1,37 +1,28 @@
+import path from "path";
 import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/authRoutes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
-import dotenv from "dotenv";
-import connectmongoose from "./db/connectmongoose.js";
+
+import connectToMongoDB from "./db/connectmongoose.js";
 import { app, server } from "./socket/socket.js";
-import cookieParser from "cookie-parser";
-import cors from "cors";
+
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-// Middleware
-app.use(cors({
-  origin: "http://localhost:3000",
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
 app.use(express.json());
 app.use(cookieParser());
-// Test route
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
 
-// Route setup
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
-// Start server and connect to MongoDB
+
+
 server.listen(PORT, () => {
-  connectmongoose();
-  console.log("Connected to mongoose function");
-  console.log(`Server running on port ${PORT}`);
+  connectToMongoDB();
+  console.log(`Server Running on port ${PORT}`);
 });
