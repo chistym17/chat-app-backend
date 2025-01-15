06612c7dc579c8a98b3825e49cb63a4c30173d2e
chistym17 <mchisty31@gmail.com>
2025-01-15 23:42:37 +0600

    modify

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,5 @@
 import express from "express";
-import protectRoute from "../middleware/protectRoute.js";
 import { getUserForSideBar } from "../controllers/user.controller.js";
 const router = express.Router();
-router.get("/", protectRoute, getUserForSideBar);
+router.get("/", getUserForSideBar);
 export default router;
