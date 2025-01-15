bcec2a4223e234c7cee319212fb300f367671417
chistym17 <mchisty31@gmail.com>
2025-01-15 21:15:57 +0600

    cors updated

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
+import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
@@ -13,6 +13,11 @@ import { app, server } from "./socket/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
+
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true
+}))
 
 app.use(express.json());
 app.use(cookieParser());
